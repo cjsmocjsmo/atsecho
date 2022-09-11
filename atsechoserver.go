c6dc789321f942cb18ac1294ea130b20fa27990f
Charlie Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2022-09-11 04:29:00 -0700

    added test server

--- atsechoserver.go
+++ atsechoserver.go
@@ -4,12 +4,30 @@ import (
 	"net/http"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
-	"golang.org/x/crypto/acme/autocert"
+	// "golang.org/x/crypto/acme/autocert"
 )
 
+// MAIN SSL SERVER 
+// func main() {
+// 	e := echo.New()
+// 	e.AutoTLSManager.Cache = autocert.DirCache("/var/www/.cache")
+// 	e.Use(middleware.CORS())
+// 	e.Use(middleware.Recover())
+// 	e.Use(middleware.Logger())
+// 	e.Static("/", "atsflutter")
+// 	e.GET("/status", func(c echo.Context) error {
+// 		return c.HTML(http.StatusOK, `
+// 			<h1>ATSECHO IS UP AND RUNNING</h1>
+// 		`)
+// 	})
+
+// 	e.Logger.Fatal(e.StartAutoTLS(":443"))
+// }
+
+// THIS IS A TEST SERVER USE THE ONE ABOVE
 func main() {
 	e := echo.New()
-	e.AutoTLSManager.Cache = autocert.DirCache("/var/www/.cache")
+	e.Use(middleware.CORS())
 	e.Use(middleware.Recover())
 	e.Use(middleware.Logger())
 	e.Static("/", "atsflutter")
@@ -18,7 +36,10 @@ func main() {
 			<h1>ATSECHO IS UP AND RUNNING</h1>
 		`)
 	})
-
-	e.Logger.Fatal(e.StartAutoTLS(":443"))
-}
-
+	// add middleware and routes
+	// ...
+	if err := e.Start(":8080"); err != http.ErrServerClosed {
+		e.Logger.Fatal(err)
+	}
+  }
+  
