6dd87607f00bd122f3bb70abb77ccf2084049d8e
Charlie Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2022-09-10 20:50:59 -0700

    update

--- atsechoserver.go
+++ atsechoserver.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"crypto/tls"
-	"golang.org/x/crypto/acme"
+	// "crypto/tls"
+	// "golang.org/x/crypto/acme"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
@@ -17,49 +17,51 @@ func main() {
 	e.AutoTLSManager.Cache = autocert.DirCache("/var/www/.cache")
 	e.Use(middleware.Recover())
 	e.Use(middleware.Logger())
+	e.Static("/", "atsflutter")
+
 	e.GET("/", func(c echo.Context) error {
 		return c.HTML(http.StatusOK, `
 			<h1>Welcome to Echo!</h1>
-			<h3>TLS certificates automatically installed from Let's Encrypt :)</h3>
+			<h3>TLS certificates automatically :_) installed from Let's Encrypt :)</h3>
 		`)
 	})
 
 	e.Logger.Fatal(e.StartAutoTLS(":443"))
 }
 
-func customHTTPServer() {
-	e := echo.New()
-	e.Use(middleware.Recover())
-	e.Use(middleware.Logger())
-	e.Static("/", "atsflutter")
-	e.GET("/status", func(c echo.Context) error {
-		return c.HTML(http.StatusOK, `
-			<h1>Welcome to ATSFlutter!</h1>
-			<h3>TLS certificates automatically installed from Let's Encrypt from custom:)</h3>
-		`)
+// func customHTTPServer() {
+// 	e := echo.New()
+// 	e.Use(middleware.Recover())
+// 	e.Use(middleware.Logger())
+// 	e.Static("/", "atsflutter")
+// 	e.GET("/status", func(c echo.Context) error {
+// 		return c.HTML(http.StatusOK, `
+// 			<h1>Welcome to ATSFlutter!</h1>
+// 			<h3>TLS certificates automatically installed from Let's Encrypt from custom:)</h3>
+// 		`)
 	
-	})
+// 	})
 
-	autoTLSManager := autocert.Manager{
-		Prompt: autocert.AcceptTOS,
-		// Cache certificates to avoid issues with rate limits (https://letsencrypt.org/docs/rate-limits)
-		Cache: autocert.DirCache("/var/www/.cache"),
-		//HostPolicy: autocert.HostWhitelist("<DOMAIN>"),
-	}
-	s := http.Server{
-		Addr:    ":443",
-		Handler: e, // set Echo as handler
-		TLSConfig: &tls.Config{
-			//Certificates: nil, // <-- s.ListenAndServeTLS will populate this field
-			GetCertificate: autoTLSManager.GetCertificate,
-			NextProtos:     []string{acme.ALPNProto},
-		},
-		//ReadTimeout: 30 * time.Second, // use custom timeouts
-	}
-	if err := s.ListenAndServeTLS("", ""); err != http.ErrServerClosed {
-		e.Logger.Fatal(err)
-	}
-}
+// 	autoTLSManager := autocert.Manager{
+// 		Prompt: autocert.AcceptTOS,
+// 		// Cache certificates to avoid issues with rate limits (https://letsencrypt.org/docs/rate-limits)
+// 		Cache: autocert.DirCache("/var/www/.cache"),
+// 		//HostPolicy: autocert.HostWhitelist("<DOMAIN>"),
+// 	}
+// 	s := http.Server{
+// 		Addr:    ":443",
+// 		Handler: e, // set Echo as handler
+// 		TLSConfig: &tls.Config{
+// 			//Certificates: nil, // <-- s.ListenAndServeTLS will populate this field
+// 			GetCertificate: autoTLSManager.GetCertificate,
+// 			NextProtos:     []string{acme.ALPNProto},
+// 		},
+// 		//ReadTimeout: 30 * time.Second, // use custom timeouts
+// 	}
+// 	if err := s.ListenAndServeTLS("", ""); err != http.ErrServerClosed {
+// 		e.Logger.Fatal(err)
+// 	}
+// }
 
 
 
